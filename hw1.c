e89053796f8269e25c613a1d189d23d035b043ab
Kaushik Prakash <k2.prakash@gmail.com>
2016-09-10 23:36:03 -0600

    Added debug logs in hw1.c Added check for length in main.c

--- hw1.c
+++ hw1.c
@@ -11,19 +11,19 @@ char reverse (char * str, int length) {
  char tmp = 0;
 
  if (sizeof_str(str) == 0) { // check if the passed string has a zero length
- 	printf("\nString of 0 length passed\n");
+ 	printf("\nString of 0 length passed. Invalid string passed.\n");
         return -5;
  }  else if (!(*str)) { // check to see if the string passed is null
         printf("\nNull string passed\n");
         return -1;
  } else if (length < 0 ) { //check to see if the passed length is negative
-        printf("\nNegative string length passed\n");
+        printf("\nNegative string length passed. Invalid length passed\n");
         return -2;
  } else if (length == 0) { // check to see if the passed length is 0
-        printf("\nString of zero length passed\n");
+        printf("length passed is 0. Invalid length passed.\n");
         return -3;
  }  else if (length > sizeof_str(str)) { // check to see if the passed length is greater than the length of the string
-         printf("\nLength greater than length of the string\n");
+         printf("\nLength greater than length of the string. Invalid length passed.\n");
          return -4;
  } else {
  	while (*str != '\0') {
