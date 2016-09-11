e2252f6a9b160a57ce9c6e6eecfbc98dae4d7218
Kaushik Prakash <k2.prakash@gmail.com>
2016-09-10 23:05:01 -0600

    Added extern <function declaration> in main.c to remove the warnings during compilation In hw1.c, added sizeof_str function

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include "hw1.h"
+
+extern char reverse (char * str, int length);
+extern int sizeof_str(char * str);
+
 int main(){
-  printf("Hello World\n");
+    //printf("Hello World\n");
+    char str[] = "Hello World";
+    int str_size = 0;
+    int rc = 0;
+    printf("%s\n", str);
+    str_size = sizeof_str(str);
+    printf("Size of the string is %d bytes\n", str_size);
+    rc = reverse(str, 5);
+    if (rc == 0) {
+	printf("%s\n", str);
+    }
+
   return 0;
 }
 
