93b4b39fc18e77b1e7556e87b65ad1e274e4b837
Kaushik Prakash <k2.prakash@gmail.com>
2016-09-10 22:39:43 -0600

    Created a new file hw1.c and added the declaration of sizeof_str function in the hw1.h header file

---
+++ hw1.c
@@ -0,0 +1,54 @@
+#include "hw1.h"
+
+
+/*
+* This fucntion will perform a string reversal upto the given length of the string
+*/
+char reverse (char * str, int length) {
+ int c = 0;
+ int l = length;
+ char tmp = 0;
+
+ if (sizeof_str(str) == 0) { // check if the passed string has a zero length
+ 	printf("\nString of 0 length passed\n");
+        return -5;
+ }  else if (!(*str)) { // check to see if the string passed is null
+        printf("\nNull string passed\n");
+        return -1;
+ } else if (length < 0 ) { //check to see if the passed length is negative
+        printf("\nNegative string length passed\n");
+        return -2;
+ } else if (length == 0) { // check to see if the passed length is 0
+        printf("\nString of zero length passed\n");
+        return -3;
+ }  else if (length > sizeof_str(str)) { // check to see if the passed length is greater than the length of the string
+         printf("\nLength greater than length of the string\n");
+         return -4;
+ } else {
+ 	while (*str != '\0') {
+            if (c == l/2) {
+                break;
+            } 
+            tmp = *(str+c);
+            *( str+c ) = *( str+length-1);
+            *(str+length-1) = tmp;
+            c++;
+            length--;
+  	}
+    return 0;
+
+    }
+}
+
+/*
+* This function will determine the length of the passed string
+*/
+int sizeof_str(char * str) {
+    int count = 0;
+    while (*str != '\0') {
+        count++;
+        str+=1;
+    }
+    return count;
+}
+
--- hw1.h
+++ hw1.h
@@ -1,4 +1,6 @@
 #ifndef _HW1_H
 #define _HW1_H
 char reverse_string(char * str, int length);
+int sizeof_str(char * str);
+
 #endif
